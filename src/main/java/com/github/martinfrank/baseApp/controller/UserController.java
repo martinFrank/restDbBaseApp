74e74e3868e7faa5fc3662f177fa246eb120459c
Martin Frank <martin.frank.privat@googlemail.com>
2022-03-25 08:42:10 +0100

    added swagger ui support

--- src/main/java/com/github/martinfrank/baseApp/controller/UserController.java
+++ src/main/java/com/github/martinfrank/baseApp/controller/UserController.java
@@ -3,6 +3,9 @@ package com.github.martinfrank.baseApp.controller;
 
 import com.github.martinfrank.baseApp.model.User;
 import com.github.martinfrank.baseApp.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class UserController {
 
     @Autowired
@@ -26,6 +30,11 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getById(id));
     }
 
+    @ApiOperation(value = "Add a new user", tags = { "user" })
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "User created"),
+            @ApiResponse(code = 400, message = "Invalid input"),
+            @ApiResponse(code = 409, message = "Contact already exists") })
     @PostMapping("/users")
     public ResponseEntity<User> create(@RequestBody User product) {
         return ResponseEntity.ok().body(this.userService.create(product));
